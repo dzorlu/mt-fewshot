3e5a629ca39aa3b49f2e7baff3a1586d42abc7fa
deniz <dzorlu@gmail.com>
2022-07-16 23:33:01 -0700

    config change

--- train/run_train.sh
+++ train/run_train.sh
@@ -4,20 +4,18 @@ deepspeed ./train.py \
     --model_name_or_path bigscience/T0_3B \
     --do_eval \
     --deepspeed t0ds-config.json \
-    --train_file ../data/train_answer_generation.json \
-    --validation_file ../data/test_answer_generation.json \
+    --train_file ../data/train_response_generation.json \
+    --validation_file ../data/test_response_generation.json \
     --text_column prompt \
     --target_column output \
     --output_dir output \
-    --per_device_train_batch_size=1 \
-    --per_device_eval_batch_size=1 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
     --gradient_accumulation_steps 36\
     --gradient_checkpointing \
     --learning_rate 5e-05 \
     --predict_with_generate \
     --save_total_limit 10\
-    --max_train_samples 2 \
-    --max_eval_samples 2 \
     --evaluation_strategy steps\
     --num_train_epochs 1\
     --load_best_model_at_end\
