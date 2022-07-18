c9c9686b168238364e4a9d19aa9a48dc90dd95ae
deniz <dzorlu@gmail.com>
2022-07-17 22:06:15 -0700

    smaller model

--- train/run_train.sh
+++ train/run_train.sh
@@ -2,7 +2,7 @@
 export WANDB_PROJECT=mt-fewshot
 deepspeed ./train.py \
     --model_name_or_path bigscience/T0_3B \
-    --deepspeed ../t0ds-config.json \
+    --deepspeed t0ds-config.json \
     --do_eval \
     --do_train \
     --train_file ../data/train_response_generation.json \
