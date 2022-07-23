e84935227a7b707c7dd698a7020443309c966d20
deniz <dzorlu@gmail.com>
2022-07-22 22:27:00 -0700

    cpu offload

--- train/run_train.sh
+++ train/run_train.sh
@@ -9,8 +9,8 @@ python ./train.py \
     --text_column prompt \
     --target_column output \
     --output_dir output \
-    --per_device_train_batch_size=64 \
-    --per_device_eval_batch_size=64 \
+    --per_device_train_batch_size=32 \
+    --per_device_eval_batch_size=32 \
     --gradient_checkpointing \
     --learning_rate 5e-05 \
     --predict_with_generate \
