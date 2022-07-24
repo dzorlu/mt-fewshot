92481a824ac4c037256817c5343d0a05587815d6
deniz <dzorlu@gmail.com>
2022-07-23 22:31:17 -0700

    introduce deepspeed

--- train/run_train.sh
+++ train/run_train.sh
@@ -1,14 +1,16 @@
 
 export WANDB_PROJECT=mt-fewshot
-python ./train.py \
+
+deepspeed ./train.py \
     --model_name_or_path yuchenlin/BART0pp \
+    --deepspeed t0ds-config.json \
     --do_eval \
     --do_train \
     --train_file ../data/train_response_generation.json \
     --validation_file ../data/test_response_generation.json \
     --text_column prompt \
     --target_column output \
-    --output_dir /hdd/mt_few \
+    --output_dir /hdd/mt_few/run2 \
     --per_device_train_batch_size=32 \
     --per_device_eval_batch_size=32 \
     --gradient_checkpointing \
@@ -19,8 +21,8 @@ python ./train.py \
     --num_train_epochs 1\
     --load_best_model_at_end\
     --metric_for_best_model f1 \
-    --save_steps 1000\
-    --eval_steps 1000\
+    --save_steps 2000\
+    --eval_steps 2000\
     --logging_steps 25 \
     --fp16 \
     --report_to wandb
@@ -31,4 +33,5 @@ python ./train.py \
 # --model_name_or_path bigscience/T0_3B \
 #--gradient_accumulation_steps 36\
 # deepspeed ./train.py \
-#     --deepspeed t0ds-config.json \
+# python ./train.py \
+#     
